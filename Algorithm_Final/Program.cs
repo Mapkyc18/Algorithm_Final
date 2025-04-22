34f79416cb9df0d2d405dc40b530f46ed1a6381a
Marcus Galdino <marcusgaldino3@gmail.com>
2025-04-22 14:22:45 -0600

    Console Transferred From the Old Trash File with Fixed Erros -_-...

--- Algorithm_Final/Program.cs
+++ Algorithm_Final/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using Algorithm_Final;
+using System;
+using System.Collections.Generic;
+
+
+static void Main()
+{
+    Console.WriteLine("Welcome to Hero’s Quest!");
+    Console.Write("Enter your name: ");
+    string playerName = Console.ReadLine();
+
+    HeroPlayer myHero = new HeroPlayer(playerName);
+
+    Console.WriteLine($"\n{playerName}, get ready for the adventure!");
+    myHero.ShowStats();
+
+    DungeonMap gameMap = new DungeonMap();
+    ChallengeBST challengeSystem = new ChallengeBST();
+
+    foreach (int lvl in new int[] { 5, 8, 3, 12, 15, 1, 10, 18, 6, 14 })
+    {
+        challengeSystem.Insert(lvl);
+    }
+
+    Console.WriteLine("\nPress Enter to start exploring...");
+    Console.ReadLine();
+
+    gameMap.Run(myHero, challengeSystem);
+
+    Console.WriteLine("\nEnd of Quest. Press Enter to close.");
+    Console.ReadLine();
+}
